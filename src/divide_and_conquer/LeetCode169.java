3387ce1cc216bd6523d75945ee371451f8005505
mervynlam <mervynlam@yeah.net>
2020-07-24 18:00:12 +0800

    update

---
+++ src/divide_and_conquer/LeetCode169.java
@@ -0,0 +1,37 @@
+package divide_and_conquer;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/majority-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode169 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.next();
+            String[] inputArr = input.split(",");
+            int[] nums = new int[inputArr.length];
+            for (int i = 0; i < inputArr.length; ++i) {
+                nums[i] = Integer.valueOf(inputArr[i]);
+            }
+            int ret = maxSubArray(nums);
+            System.out.println(ret);
+        }
+    }
+
+    /**
+     *
+     */
+    public static int maxSubArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
--- src/dynamic_programming/LeetCode1314.java
+++ src/dynamic_programming/LeetCode1314.java
@@ -1,4 +1,4 @@
-package dp;
+package dynamic_programming;
 
 import java.util.Arrays;
 import java.util.Scanner;
