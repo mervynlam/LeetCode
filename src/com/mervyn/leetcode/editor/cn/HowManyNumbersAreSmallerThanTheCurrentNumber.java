dd1374390b3e5ef29b31a741202c0330c960c2d6
mervynlam <mervynlam@yeah.net>
2020-10-28 14:26:58 +0800

    leetcode1365

--- src/com/mervyn/leetcode/editor/cn/HowManyNumbersAreSmallerThanTheCurrentNumber.java
+++ src/com/mervyn/leetcode/editor/cn/HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -58,14 +58,27 @@ public class HowManyNumbersAreSmallerThanTheCurrentNumber {
             int[] count = new int[101];
             for (int num : nums)
                 count[num]++;
-            for (int i = 0; i < nums.length; ++i) {
-                for (int j = 0; j < nums[i]; ++j) {
-                    result[i] += count[j];
-                }
-            }
+            for (int i = 1; i <= 100; ++i)
+                count[i] += count[i-1];
+            for (int i = 0; i < nums.length; ++i)
+                result[i] = nums[i] == 0? 0 : count[nums[i]-1];
             return result;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
+//    class Solution {
+//        public int[] smallerNumbersThanCurrent(int[] nums) {
+//            int[] result = new int[nums.length];
+//            int[] count = new int[101];
+//            for (int num : nums)
+//                count[num]++;
+//            for (int i = 0; i < nums.length; ++i) {
+//                for (int j = 0; j < nums[i]; ++j) {
+//                    result[i] += count[j];
+//                }
+//            }
+//            return result;
+//        }
+//    }
 }
